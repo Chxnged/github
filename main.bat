67bd2393070fd40acb756039f9e9e78955a412a8
Changed <changed.duke@gmail.com>
2026-01-17 19:23:08 +0100

    Update main.bat

--- main.bat
+++ main.bat
@@ -2,15 +2,7 @@
 title VRModz APK
 color 5
 echo.
+timeout /t 1 >nul
 echo Beta not released
-echo.
-
-for /l %%i in (5,-1,1) do (
-    echo Closing in %%i seconds...
-    timeout /t 1 >nul
-    cls
-    echo Beta not released
-    echo.
-)
 
 exit
